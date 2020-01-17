60e8db85b69ef41edf65a1267fbfd0e6ae314e88
Wermeson Rocha <wermeson.rocha@ledstartup.studio>
2020-01-17 09:45:48 -0300

    Config for heroku

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://zvawuhi8pf.execute-api.sa-east-1.amazonaws.com/dev',
+  baseURL: process.env.API_URL
 });
 
 export default api;
